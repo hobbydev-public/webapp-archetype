364160c53c96cf2899f6de003fc6918c0c0e77b3
Dmytro Romenskyi <d.romenskyi@gmail.com>
2017-08-16 18:56:23 +0300

    app.module.js changed

--- src/main/webapp/WEB-INF/res/app/js/es6/app.module.js
+++ src/main/webapp/WEB-INF/res/app/js/es6/app.module.js
@@ -1,19 +1,38 @@
+// custom resources
 import './grid.css';
 
+// vendor modules
 import angular from 'angular';
 import route from 'angular-route';
+import resource from 'angular-resource';
+import uigrid from 'angular-ui-grid';
+import uibootstrap from 'angular-ui-bootstrap';
+import animate from 'angular-animate';
 
+// app utility modules
 import components from './components';
 import modals from './modals';
+import services from './services';
 
-import usersModule from './modules/users';
+// app modules
+import dashboard from './modules/dashboard';
+import profile from './modules/profile';
 
+// routing
 import routesConfig from './app.module.routing';
 
 let rootModule = angular.module('app',
 	[
-		route, 'angular-password',
-		components, modals,
-		usersModule
+		route, 'angular-password', resource, uibootstrap, animate, uigrid, 'ui.calendar', 'ui-iconpicker', 'ngSanitize', 'ngCsv',
+		components, modals, services,
+		dashboard, profile
 	])
+	.controller('rootCtrl', function rootCtrl($scope, principalService) {
+
+		principalService.getCurrentUser(function (currentUser) {
+			$scope.$root.appContext = {
+				currentUser: currentUser
+			};
+		});
+	})
 	.config(routesConfig);
