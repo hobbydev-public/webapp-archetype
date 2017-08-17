51595e3929dcde6d956bbb5d2b19eefd133af4d9
Dmytro Romenskyi <d.romenskyi@gmail.com>
2017-08-17 17:31:06 +0300

    fe api models package updated

---
+++ src/main/java/groupId/artifactId/api/models/fe/PasswordsView.java
@@ -0,0 +1,23 @@
+package groupId.artifactId.api.models.fe;
+
+public class PasswordsView {
+	
+	private String oldPass;
+	private String newPass;
+	
+	public String getOldPass() {
+		return oldPass;
+	}
+	
+	public void setOldPass(String oldPass) {
+		this.oldPass = oldPass;
+	}
+	
+	public String getNewPass() {
+		return newPass;
+	}
+	
+	public void setNewPass(String newPass) {
+		this.newPass = newPass;
+	}
+}
--- src/main/java/groupId/artifactId/api/models/fe/UserView.java
+++ src/main/java/groupId/artifactId/api/models/fe/UserView.java
@@ -1,28 +1,69 @@
-/**
- * This software is licensed under the terms of the MIT license.
- * Copyright (C) 2016 Dmytro Romenskyi
- */
 package groupId.artifactId.api.models.fe;
-        
-import groupId.artifactId.domain.users.User;
 
-public class UserView {
-    
-    private Long id;
-    
-    public UserView() {}
-    
-    public User toDomain() {
-        User domain = new User();
-        domain.setId(getId());
-        return domain;
-    }
-    
-    public Long getId() {
-        return id;
-    }
-    
-    public void setId(Long id) {
-        this.id = id;
-    }
+import groupId.artifactId.api.models.fe.generic.View;
+import groupId.artifactId.domain.users.User;
+import groupId.artifactId.utils.DateUtils;
+
+import java.util.Date;
+
+public class UserView implements View<User> {
+	
+	private Long id;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	
+	@Override
+	public User toDomain() {
+		User domain = new User();
+		
+		domain.setId(this.id);
+		domain.setEmail(this.email);
+		domain.setFirstName(this.firstName);
+		domain.setLastName(this.lastName);
+		domain.setBirthDate(DateUtils.toLocalDate(this.birthDate));
+		
+		return domain;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
 }
